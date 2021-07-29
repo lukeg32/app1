6e19d68fd16e27c838eb10ea4d23160a5eaa6ebe
lukegantar <lukegantar@gmail.com>
2021-07-28 20:06:05 -0700

    did core data init

---
+++ Stream2/MainScreenViewController.swift
@@ -0,0 +1,12 @@
+//
+//  MainScreenViewController.swift
+//  Stream2
+//
+//  Created by Luke Gantar on 7/28/21.
+//
+
+import UIKit
+
+class MainScreenViewController: UITableViewController {
+    
+}
