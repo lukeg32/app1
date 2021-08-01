61d6774c2a4f7d31677b54b02fad736fa375ff8e
lukegantar <lukegantar@gmail.com>
2021-08-01 16:29:24 -0700

    has basic time filter, and date formatting based off of the filtering

--- Stream2/ReminderListViewController.swift
+++ Stream2/ReminderListViewController.swift
@@ -1,24 +1,22 @@
-//
-//  ViewController.swift
-//  Stream2
-//
-//  Created by Luke Gantar on 7/11/21.
-//
-
 import UIKit
 
 class ReminderListViewController: UITableViewController {
-    private var reminderListDataSource: ReminderListDataSource?
+    @IBOutlet var filterSegmentedControl: UISegmentedControl!
     
     static let showDetailSegueIdentifier = "ShowReminderDetailSegue"
     static let mainStoryboardName = "Main"
-        static let detailViewControllerIdentifier = "ReminderDetailViewController"
-    
+    static let detailViewControllerIdentifier = "ReminderDetailViewController"
+
+    private var reminderListDataSource: ReminderListDataSource?
+    private var filter: ReminderListDataSource.Filter {
+        return ReminderListDataSource.Filter(rawValue: filterSegmentedControl.selectedSegmentIndex) ?? .today
+    }
+
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == Self.showDetailSegueIdentifier,
-           let destination = segue.destination as? ReminderDetailViewController,
-           let cell = sender as? UITableViewCell,
-           let indexPath = tableView.indexPath(for: cell) {
+            let destination = segue.destination as? ReminderDetailViewController,
+            let cell = sender as? UITableViewCell,
+            let indexPath = tableView.indexPath(for: cell) {
             let rowIndex = indexPath.row
             guard let reminder = reminderListDataSource?.reminder(at: rowIndex) else {
                 fatalError("Couldn't find data source for reminder list.")
@@ -29,25 +27,30 @@ class ReminderListViewController: UITableViewController {
             })
         }
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         reminderListDataSource = ReminderListDataSource()
         tableView.dataSource = reminderListDataSource
     }
-    
+
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         if let navigationController = navigationController,
-           navigationController.isToolbarHidden {
+            navigationController.isToolbarHidden {
             navigationController.setToolbarHidden(false, animated: animated)
         }
     }
-    
-    @IBAction func addButtonTriggered(_ sender: Any) {
+
+    @IBAction func addButtonTriggered(_ sender: UIBarButtonItem) {
         addReminder()
     }
     
+    @IBAction func segmentControlChanged(_ sender: UISegmentedControl) {
+        reminderListDataSource?.filter = filter
+        tableView.reloadData()
+    }
+
     private func addReminder() {
         let storyboard = UIStoryboard(name: Self.mainStoryboardName, bundle: nil)
         let detailViewController: ReminderDetailViewController = storyboard.instantiateViewController(identifier: Self.detailViewControllerIdentifier)
@@ -58,33 +61,5 @@ class ReminderListViewController: UITableViewController {
         })
         let navigationController = UINavigationController(rootViewController: detailViewController)
         present(navigationController, animated: true, completion: nil)
-
     }
-    
-    
 }
-
-/*
-extension ReminderListViewController {
-    static let reminderListCellIdentifier = "ReminderListCell"
-    
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return Reminder.testData.count
-    }
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-            guard let cell = tableView.dequeueReusableCell(withIdentifier: Self.reminderListCellIdentifier, for: indexPath) as? ReminderListCell else {
-                fatalError("Unable to dequeue ReminderCell")
-            }
-        let reminder = Reminder.testData[indexPath.row]
-        
-        cell.configure(title: reminder.title, dateText: reminder.dueDate.description, isDone: reminder.isComplete) {
-                Reminder.testData[indexPath.row].isComplete.toggle()
-                tableView.reloadRows(at: [indexPath], with: .none)
-        }
-        return cell
-    }
-     
-}
- */
-
